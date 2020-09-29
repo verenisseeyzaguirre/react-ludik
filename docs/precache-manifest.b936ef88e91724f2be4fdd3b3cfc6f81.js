0742cac996413de5a047e8654942557d4436213c
verenisseeyzaguirre <verenisse.eyzaguirre@gmail.com>
2020-09-29 17:54:32 -0500

    docs agregado

---
+++ docs/precache-manifest.b936ef88e91724f2be4fdd3b3cfc6f81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15aa342c2044df12f9a832c595b5433d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aedecd287c2b38c9316c",
+    "url": "/static/css/main.fa377dc4.chunk.css"
+  },
+  {
+    "revision": "22f4c9b172e676a2a77f",
+    "url": "/static/js/2.4be22cbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4be22cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aedecd287c2b38c9316c",
+    "url": "/static/js/main.d2e8dd2b.chunk.js"
+  },
+  {
+    "revision": "6eafe1cc8a5185c9ef05",
+    "url": "/static/js/runtime-main.357a95b0.js"
+  }
+]);
